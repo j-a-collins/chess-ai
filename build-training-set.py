6236e4cdb5f38dd467ece2432378891828d5d41d
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2022-01-26 15:52:58 +0000

    Update build-training-set.py
    black formatting styleguide

--- build-training-set.py
+++ build-training-set.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-'''
+"""
 Training model using PNG data from database.lichess.org
 processes
 
 Last write
 user: j-a-collins
 date: 08-02-21
-'''
+"""
 
 # # # imports
 import chess.pgn
@@ -16,26 +16,28 @@ import os
 import numpy as np
 
 
-def get_training_data(number_of_samples = None):
-    '''
+def get_training_data(number_of_samples=None):
+    """
     A function to get all the PGNs from a specified folder
     and analyse the data for points values and then process
     them. It then returns two arrays.
-    '''
+    """
     x, y = [], []
     game_notation = 0
-    points = {'1/2-1/2':0, '0-1':-1, '1-0':1}
+    points = {"1/2-1/2": 0, "0-1": -1, "1-0": 1}
 
     # PGN data files sit inside the lichess-PGNs folder
     # May be better to use glob?
-    directory = 'C:\\Users\\jacka\\Documents\\chess-ai-master\\chess-ai-master\\lichess-PGNs'
+    directory = (
+        "C:\\Users\\jacka\\Documents\\chess-ai-master\\chess-ai-master\\lichess-PGNs"
+    )
     for file_name in os.listdir(directory):
         pgn = open(os.path.join(directory, file_name))
         while True:
             game = chess.pgn.read_game(pgn)
             if game is None:
                 break
-            result_of_game = game.headers['Result']
+            result_of_game = game.headers["Result"]
             if result_of_game not in points:
                 continue
 
@@ -59,8 +61,9 @@ def get_training_data(number_of_samples = None):
 
     return x, y
 
+
 if __name__ == "__main__":
-    NUMBER_OF_PGNS = 25000000 # Currently hardcoded the number of PGNs
+    NUMBER_OF_PGNS = 25000000  # Currently hardcoded the number of PGNs
     x, y = get_training_data(NUMBER_OF_PGNS)
     # Saves the arrays into an uncompressed .npz
     np.savez("processed/dataset.npz", x, y)
